183196839d59fa4f36ef717dbbbd40424686b8b5
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-10 23:12:43 -0400

    HTML routes for notes.html and index.html working

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 // Dependencies
 const express = require("express");
-const PORT = 7500;
+const path = require("path");
+
+// Establish Express App
 const app = express();
+const PORT = 7500;
 
-//HTML Routes
-// GET /notes to return notes.thml
-//GET * return index.html
+// Note data
+const notes = [];
 
-//API Routes
+//TODO: HTML Routes
+app.get("/notes.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/index.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+//TODO: API Routes
 //GET /api/notes to read db.json and retun all saved notes as JSON
 //POST /api/notes to receive a new note to save on request body and add to db.js file, then return new note to client
     //each note needs a unique id when it's saved. check out npm packages that can do this for me
+
+
+//Render notes.html
+
 
 // Listener
 app.listen(PORT, () => {
