3eb96f6b56f033184d991d8d344f673199a6d797
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-10 23:23:48 -0400

    API routes in progress

--- server.js
+++ server.js
@@ -6,25 +6,25 @@ const path = require("path");
 const app = express();
 const PORT = 7500;
 
-// Note data
-const notes = [];
-
-//TODO: HTML Routes
+//HTML Routes
 app.get("/notes.html", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
 app.get("/index.html", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
 //TODO: API Routes
-//GET /api/notes to read db.json and retun all saved notes as JSON
+//GET /api/notes to read db.json and return all saved notes as JSON
+app.get("/api/notes", (req, res) => {
+    
+});
+
 //POST /api/notes to receive a new note to save on request body and add to db.js file, then return new note to client
-    //each note needs a unique id when it's saved. check out npm packages that can do this for me
-
-
-//Render notes.html
+app.POST("/api/notes", (req, res) => {
+    
+});
+//each note needs a unique id when it's saved. check out npm packages that can do this for me
 
 
 // Listener
