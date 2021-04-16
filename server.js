28fcb0384bd8889bfa1eb398337bd1f1ddeb7804
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-15 20:16:43 -0400

    Corrected UI display; app.post in API routes functional

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const util = require('util');
+const { uuid } = require('uuidv4');
+
+const readFileSync = util.promisify(fs.readFile);
+const writeFileSync = util.promisify(fs.writeFile);
 
 // Establish Express App
 const app = express();
@@ -24,21 +29,42 @@ app.get("/index", (req, res) => {
 //GET /api/notes to read db.json and return all saved notes as JSON
 app.get("/api/notes", (req, res) => {
     //use fs to read db.json
+    readFileSync('db/db.json', 'utf8').then(data => {
+        res.json(JSON.parse(data));
+    });
 });
 
 //POST /api/notes to receive a new note to save on request body and add to db.js file, then return new note to client
 // TODO: each note needs a unique id when it's saved. check out npm packages that can do this for me
 app.post("/api/notes", (req, res) => {
     //use fs to read data and store to notesArray
+    readFileSync('db/db.json', 'utf8').then(data => {
+       const notesArray = JSON.parse(data);
+       const newNote = req.body;
+       newNote.id = uuid();
+       
+       console.log(newNote);
+       notesArray.push(newNote);
+   
+       //use fs to rewrite to db.json w/ updated notesArray
+       writeFileSync('db/db.json', JSON.stringify(notesArray));
 
-    const newNote = req.body;
-    newNote.id = id;
-    
-    console.log(newNote);
-    notesArray.push(newNote);
+        res.json(newNote);
+    });
+});
 
-    //use fs to rewrite to db.json w/ updated notesArray
-    res.json(newNote);
+app.delete("/api/notes/:id", (req, res) => {
+    //use fs to read data and store to notesArray
+    readFileSync('db/db.json', 'utf8').then(data => {
+       const notesArray = JSON.parse(data);
+      
+       // how to remove the note with req.params.id === note.id
+   
+       //use fs to rewrite to db.json w/ updated notesArray
+       writeFileSync('db/db.json', JSON.stringify(notesArray));
+
+        res.json(newNote);
+    });
 });
 
 // Listener
